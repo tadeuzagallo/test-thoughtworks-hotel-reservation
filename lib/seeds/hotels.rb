65289a2076ee24c466911f15dc63e0140eb4c914
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-02-14 01:54:47 -0200

    fix parentheses

--- lib/seeds/hotels.rb
+++ lib/seeds/hotels.rb
@@ -4,29 +4,29 @@
 
 require_relative '../hotel_helper'
 
-HotelHelper.register_hotel Hotel.new.tap do |h|
+HotelHelper.register_hotel(Hotel.new.tap do |h|
   h.name = 'Lakewood'
   h.rating = 3
   h.add_price(:regular, false, 110)
   h.add_price(:rewards, false,  80)
   h.add_price(:regular,  true,  90)
   h.add_price(:rewards,  true,  80)
-end
+end)
 
-HotelHelper.register_hotel Hotel.new.tap do |h|
+HotelHelper.register_hotel(Hotel.new.tap do |h|
   h.name = 'Bridgewood'
   h.rating = 4
   h.add_price(:regular, false, 160)
   h.add_price(:rewards, false, 110)
   h.add_price(:regular,  true,  60)
   h.add_price(:rewards,  true,  50)
-end
+end)
 
-HotelHelper.register_hotel Hotel.new.tap do |h|
+HotelHelper.register_hotel(Hotel.new.tap do |h|
   h.name = 'Ridgewood'
   h.rating = 5
   h.add_price(:regular, false, 220)
   h.add_price(:rewards, false, 100)
   h.add_price(:regular,  true, 150)
   h.add_price(:rewards,  true,  40)
-end
+end)
