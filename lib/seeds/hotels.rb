4a39537fd3fa8cd17eb766d72f8007679f949a03
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-02-14 01:32:07 -0200

    add seed to hotels

--- lib/seeds/hotels.rb
+++ lib/seeds/hotels.rb
@@ -2,3 +2,31 @@
 #● Bridgewood with a rating of 4 has weekday rates as 160$ for regular customer and 110$ for rewards customer. The weekend rates are 60$ for regular customer and 50$ for a rewards customer.
 #● Ridgewood with a rating of 5 has weekday rates as 220$ for regular customer and 100$ for rewards customer. The weekend rates are 150$ for regular customer and 40$ for a rewards customer.
 
+require_relative '../reservation_helper'
+
+ReservationHelper.register_hotel Hotel.new.tap do |h|
+  h.name = 'Lakewood'
+  h.rating = 3
+  h.add_price(:regular, false, 110)
+  h.add_price(:rewards, false,  80)
+  h.add_price(:regular,  true,  90)
+  h.add_price(:rewards,  true,  80)
+end
+
+ReservationHelper.register_hotel Hotel.new.tap do |h|
+  h.name = 'Bridgewood'
+  h.rating = 4
+  h.add_price(:regular, false, 160)
+  h.add_price(:rewards, false, 110)
+  h.add_price(:regular,  true,  60)
+  h.add_price(:rewards,  true,  50)
+end
+
+ReservationHelper.register_hotel Hotel.new.tap do |h|
+  h.name = 'Ridgewood'
+  h.rating = 5
+  h.add_price(:regular, false, 220)
+  h.add_price(:rewards, false, 100)
+  h.add_price(:regular,  true, 150)
+  h.add_price(:rewards,  true,  40)
+end
